ed409fd8ee7838d1dec0d965f8a13d3b3f04b0d6
ToshiroSama <alishdjuraev@gmail.com>
2022-12-29 09:18:13 +0500

    TableHeaderView was set up

---
+++ NetflixCloneUIKit/Views/HeroHeaderUIView.swift
@@ -0,0 +1,80 @@
+//
+//  HeroHeaderUIView.swift
+//  NetflixCloneUIKit
+//
+//  Created by Alisher Djuraev on 29/12/22.
+//
+
+import UIKit
+import SnapKit
+
+class HeroHeaderUIView: UIView {
+    
+    private let heroImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = UIImage(named: "dune")
+        return imageView
+    }()
+    
+    private let playButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Play", for: .normal)
+        button.layer.borderColor = UIColor.white.cgColor
+        button.layer.borderWidth = 1
+        button.layer.cornerRadius = 5
+        return button
+    }()
+    
+    private let downloadButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Download", for: .normal)
+        button.layer.borderColor = UIColor.white.cgColor
+        button.layer.borderWidth = 1
+        button.layer.cornerRadius = 5
+        return button
+    }()
+    
+    private func addGradient() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [
+            UIColor.clear.cgColor,
+            UIColor.systemBackground.cgColor
+        ]
+        gradientLayer.frame = bounds
+        layer.addSublayer(gradientLayer)
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(heroImageView)
+        addGradient()
+        addSubview(playButton)
+        addSubview(downloadButton)
+        setConstraints()
+    }
+    
+    private func setConstraints() {
+        playButton.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(70)
+            make.bottom.equalToSuperview().offset(-50)
+            make.width.equalTo(120)
+        }
+        
+        downloadButton.snp.makeConstraints { make in
+            make.trailing.equalToSuperview().offset(-70)
+            make.bottom.equalToSuperview().offset(-50)
+            make.width.equalTo(120)
+        }
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        heroImageView.frame = bounds
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+}
