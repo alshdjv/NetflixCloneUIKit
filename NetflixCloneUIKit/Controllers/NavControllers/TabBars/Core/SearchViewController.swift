a38f14d2daa6ca877fc5407d93b81015a2c8e982
ToshiroSama <alishdjuraev@gmail.com>
2023-01-11 11:33:23 +0500

    SearchViewController is totally finished

---
+++ NetflixCloneUIKit/Controllers/NavControllers/TabBars/Core/SearchViewController.swift
@@ -0,0 +1,117 @@
+//
+//  SearchViewController.swift
+//  NetflixCloneUIKit
+//
+//  Created by Alisher Djuraev on 28/12/22.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+    private var titles: [Title] = [Title]()
+    
+    private let discoverTable: UITableView = {
+        let tableView = UITableView()
+        tableView.register(TitleTableViewCell.self, forCellReuseIdentifier: TitleTableViewCell.identifier)
+        return tableView
+    }()
+    
+    private let searchController: UISearchController = {
+        let searchController = UISearchController(searchResultsController: SearchResultsViewController())
+        searchController.searchBar.placeholder = "Search for a Movie or a Tv show"
+        searchController.searchBar.searchBarStyle = .minimal
+        return searchController
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Search"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
+        
+        setupUI()
+        
+        discoverTable.delegate = self
+        discoverTable.dataSource = self
+        
+        navigationItem.searchController = searchController
+        navigationController?.navigationBar.tintColor = .white
+        
+        fetchDiscoverMovies()
+        
+        searchController.searchResultsUpdater = self
+    }
+    
+    private func setupUI() {
+        view.addSubview(discoverTable)
+    }
+    
+    private func fetchDiscoverMovies() {
+        APICaller.shared.getDiscoverMovies { [weak self] result in
+            switch result {
+            case .success(let titles):
+                self?.titles = titles
+                DispatchQueue.main.async {
+                    self?.discoverTable.reloadData()
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        discoverTable.frame = view.bounds
+    }
+}
+
+// MARK: - Extension for SearchViewController which confirm two protocols
+
+extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return titles.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: TitleTableViewCell.identifier, for: indexPath) as? TitleTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        let title = titles[indexPath.row]
+        let model = TitleViewModel(titleName: title.original_name ?? title.original_title ?? "Unknown name", posterURL: title.poster_path ?? "Unknown")
+        cell.configure(with: model)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 180
+    }
+}
+
+extension SearchViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        let searchBar = searchController.searchBar
+        
+        guard let query = searchBar.text,
+              !query.trimmingCharacters(in: .whitespaces).isEmpty,
+              query.trimmingCharacters(in: .whitespaces).count >= 3,
+              let resultsController = searchController.searchResultsController as? SearchResultsViewController else {
+            return
+        }
+        
+        APICaller.shared.search(with: query) { result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let titles):
+                    resultsController.titles = titles
+                    resultsController.searchResultsCollectionView.reloadData()
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
+        }
+    }
+}
--- NetflixCloneUIKit/Controllers/NavControllers/TabBars/Core/UpcomingViewController.swift
+++ NetflixCloneUIKit/Controllers/NavControllers/TabBars/Core/UpcomingViewController.swift
@@ -1,5 +1,5 @@
 //
-//  SearchViewController.swift
+//  UpcomingViewController.swift
 //  NetflixCloneUIKit
 //
 //  Created by Alisher Djuraev on 28/12/22.
@@ -7,11 +7,11 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
+class UpcomingViewController: UIViewController {
     
     private var titles: [Title] = [Title]()
     
-    private let discoverTable: UITableView = {
+    private let upcomingTable: UITableView = {
         let tableView = UITableView()
         tableView.register(TitleTableViewCell.self, forCellReuseIdentifier: TitleTableViewCell.identifier)
         return tableView
@@ -19,45 +19,47 @@ class SearchViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "Search"
+        // title for controller
+        title = "Upcoming Movies"
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationController?.navigationItem.largeTitleDisplayMode = .always
         
         setupUI()
         
-        discoverTable.delegate = self
-        discoverTable.dataSource = self
+        // using two patterns
+        upcomingTable.delegate = self
+        upcomingTable.dataSource = self
         
-        fetchDiscoverMovies()
+        fetchUpcoming()
     }
     
     private func setupUI() {
-        view.addSubview(discoverTable)
+        view.addSubview(upcomingTable)
     }
     
-    private func fetchDiscoverMovies() {
-        APICaller.shared.getDiscoverMovies { [weak self] result in
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        upcomingTable.frame = view.bounds
+    }
+    
+    private func fetchUpcoming() {
+        APICaller.shared.getUpcomingMovies { [weak self] result in
             switch result {
             case .success(let titles):
                 self?.titles = titles
                 DispatchQueue.main.async {
-                    self?.discoverTable.reloadData()
+                    self?.upcomingTable.reloadData()
                 }
             case .failure(let error):
                 print(error.localizedDescription)
             }
         }
     }
-    
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        discoverTable.frame = view.bounds
-    }
 }
 
-// MARK: - Extension for SearchViewController which confirm two protocols
+// MARK: - Extension for UpcomingViewController which confirm two protocols
 
-extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+extension UpcomingViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return titles.count
@@ -69,8 +71,7 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
         }
         
         let title = titles[indexPath.row]
-        let model = TitleViewModel(titleName: title.original_name ?? title.original_title ?? "Unknown name", posterURL: title.poster_path ?? "Unknown")
-        cell.configure(with: model)
+        cell.configure(with: TitleViewModel(titleName: (title.original_title ?? title.original_name) ?? "Unknow title name", posterURL: title.poster_path ?? "Unknow title name"))
         return cell
     }
     
